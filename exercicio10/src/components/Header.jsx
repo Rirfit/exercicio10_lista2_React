05c600e6dfd5125762fa54e7d5b4fdf8dafb7087
Rirfit <lilrirfit@gmail.com>
2024-10-20 11:28:49 -0300

    FC

---
+++ exercicio10/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header(){
+    return (
+        <>
+        <h3>Olá, mundo</h3>
+        <h3>Olá proa</h3>
+        <p>O proprofissão tem uma taxa de empregabilidade de aproximadamente 80%</p>
+        </>
+    )
+}export default Header
